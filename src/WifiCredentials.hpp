f8f297bd27d7e2f61a7427f2cb9e26c7ebceec7b
n1kelodeon <75397148+n1kelodeon@users.noreply.github.com>
2021-11-07 01:51:05 +0100

    Move wifi credentials to seperate header filer

---
+++ src/WifiCredentials.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace WifiCredentials {
+static const std::string ssid = "YOUR SSID HERE";
+static const std::string password = "YOUR PASSWORD HERE";
+}  // namespace WifiCredentials
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Network.hpp"
 #include "Pattern.hpp"
 #include "RaveLights.hpp"
+#include "WifiCredentials.hpp"
 
 #include <Arduino.h>
 #include <FastLED.h>
@@ -10,16 +11,20 @@ const unsigned LED_ROW_COUNT = 30;
 const unsigned LED_COLUMN_COUNT = 5;
 
 // Vector of shared_ptr's to Pattern Instances that will be added to the RaveLights instance
-std::vector<std::shared_ptr<Pattern::AbstractPattern>> patterns{std::make_shared<Pattern::RandomSegments>(),
-                                                                std::make_shared<Pattern::RandomSequence>(),
-                                                                std::make_shared<Pattern::SingleStrobeFlash>()};
+std::vector<std::shared_ptr<Pattern::AbstractPattern>> patterns{
+    std::make_shared<Pattern::RandomSegments>(),    // 0
+    std::make_shared<Pattern::RandomSequence>(),    // 1
+    std::make_shared<Pattern::SingleStrobeFlash>()  // 2
+};
 
 // RaveLights instance
 RaveLights<LED_ROW_COUNT, LED_COLUMN_COUNT> raveLights;
 
 void setup() {
     Serial.begin(115200);
-    Network::initWifiAccessPoint("esp32", "ravelights");
+    randomSeed(analogRead(2));
+    // Network::initWifiAccessPoint(WifiCredentials::ssid, WifiCredentials::password);
+    Network::connectToWifi(WifiCredentials::ssid, WifiCredentials::password);
 
     for (auto &pattern : patterns) {
         raveLights.addPattern(pattern);
