434763e3d0412681ecbf767dd97d843f68c293e9
n1kelodeon <75397148+n1kelodeon@users.noreply.github.com>
2022-04-30 19:31:52 +0200

    twinkle: Improve for many LEDs

--- src/patterns/Twinkle.cpp
+++ src/patterns/Twinkle.cpp
@@ -2,19 +2,23 @@
 
 namespace Pattern {
 unsigned Twinkle::perform(std::vector<CRGB> &leds, CRGB color) {
+
     unsigned ledCount = rowCount_ * columnCount_;
-    unsigned pixelIndex = random(ledCount);
-    // Always light up chosen pixel
-    leds[pixelIndex] = color;
-    // Light up neighboring pixels with 50% probability each
-    if (random(0, 2) == 1) {
-        leds[(pixelIndex - 1) % (ledCount)] = color;
-    }
-    if (random(0, 2) == 1) {
-        leds[(pixelIndex + 1) % ledCount] = color;
+    unsigned spotCount = random(5, 50);
+    for (unsigned i = 0; i < spotCount; i++) {
+        unsigned pixelIndex = random(ledCount);
+        // Always light up chosen pixel
+        leds[pixelIndex] = color;
+        // Light up neighboring pixels with 50% probability each
+        if (sampleBernoulli(0.5)) {
+            leds[(pixelIndex - 1) % (ledCount)] = color;
+        }
+        if (sampleBernoulli(0.5)) {
+            leds[(pixelIndex + 1) % ledCount] = color;
+        }
     }
     FastLED.show();
-    unsigned offDuration = random(0, 10);
+    unsigned offDuration = random(0, 2);
     return offDuration;
 }
 };  // namespace Pattern
